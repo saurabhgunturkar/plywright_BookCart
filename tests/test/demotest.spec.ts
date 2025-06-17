4847b0ad041504d13c12571f7dbab1d190f12f9f
saurabhgunturkar <saurabhgunturkar07@gmail.com>
2025-06-17 16:30:54 +0530

    chnage 12

--- tests/test/demotest.spec.ts
+++ tests/test/demotest.spec.ts
@@ -1,7 +1,7 @@
 import test from "playwright/test";
 
 
-test('Demotest', async ({ page }) => {
+test('Demotest @smoke', async ({ page }) => {
     await page.goto('https://example.com');
     await page.screenshot({ path: 'screenshots/example.png' });
     const title = await page.title();
@@ -9,6 +9,35 @@ test('Demotest', async ({ page }) => {
     // await page.pause();  // Pauses the test execution and opens the Playwright Inspector
     const heading = await page.locator('h1').innerText();
     console.log("Heading:", heading);
+    console.log("================ Smoke Test Completed ================");
+
+    // Add any additional assertions or interactions here
+
+});
+
+test('Demotest @regression', async ({ page }) => {
+    await page.goto('https://example.com');
+    await page.screenshot({ path: 'screenshots/example.png' });
+    const title = await page.title();
+    console.log("Page Title:", title);
+    // await page.pause();  // Pauses the test execution and opens the Playwright Inspector
+    const heading = await page.locator('h1').innerText();
+    console.log("Heading:", heading);
+    console.log("================ Regression Test Completed ================");
+
+    // Add any additional assertions or interactions here
+
+});
+
+test('Demotest @sanity', async ({ page }) => {
+    await page.goto('https://example.com');
+    await page.screenshot({ path: 'screenshots/example.png' });
+    const title = await page.title();
+    console.log("Page Title:", title);
+    // await page.pause();  // Pauses the test execution and opens the Playwright Inspector
+    const heading = await page.locator('h1').innerText();
+    console.log("Heading:", heading);
+    console.log("================ Sanity Test Completed ================");
     // Add any additional assertions or interactions here
 
 });
