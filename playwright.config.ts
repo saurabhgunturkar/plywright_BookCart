ab8508d4ddc624571aaeaa44c953ee838319a482
saurabhgunturkar <saurabhgunturkar07@gmail.com>
2025-07-10 01:28:06 +0530

    update 19

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,69 +1,50 @@
-import { defineConfig, devices } from "playwright/test";
+import { defineConfig, devices } from '@playwright/test';
 
 const baseURL = process.env.PLAYWRIGHT_TEST_BASE_URL || 'https://bookcart.azurewebsites.net/';
 
 export default defineConfig({
   testDir: './tests/BookCartApp/tests',
   fullyParallel: true,
-  globalTeardown:'./tests/Utils/global-teardown.ts',
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  globalTeardown: './tests/Utils/global-teardown.ts',
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
   reporter: [['list'], ['html']],
   use: {
-    screenshot:"only-on-failure",
+    screenshot: 'only-on-failure',
     baseURL,
     trace: 'on-first-retry',
-    headless: true
+    headless: true,
   },
 
   projects: [
-    
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],
-      channel:'chrome',
-      
-      }
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome',
+      },
     },
-    
+    // Uncomment the following projects as needed
     // {
     //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox']},
+    //   use: { ...devices['Desktop Firefox'] },
     // },
     // {
     //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari']},
+    //   use: { ...devices['Desktop Safari'] },
     // },
     // {
-    //   name: 'Iphone 14',
-    //   use: { ...devices['iPhone 14']},
+    //   name: 'iPhone 14',
+    //   use: { ...devices['iPhone 14'] },
     // },
-    // // {
-    // //   name: 'Mobile Firefox',
-    // //   use: { ...devices['Pixel 5']},
-    // // },
-    // // {
-    // //   name: 'Mobile Chrome',
-    // //   use: { ...devices['Pixel 5']},
-    // // },
-    // // {
-    // //   name: 'Mobile Chrome',
-    // //   use: { ...devices['Pixel 5'] },
-    // // },
     // {
-    //   name: 'Iphone 15 pro max',
+    //   name: 'iPhone 15 Pro Max',
     //   use: { ...devices['iPhone 15 Pro Max'] },
     // },
     // {
-    //   name:'Ipad 11 pro',
-    //   use: {...devices['iPad Pro 11']}
-    // }
-    
-
-
+    //   name: 'iPad Pro 11',
+    //   use: { ...devices['iPad Pro 11'] },
+    // },
   ],
-
-
 });
